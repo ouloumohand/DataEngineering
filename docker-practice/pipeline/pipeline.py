872f7ec814d226517224f31e5b48b668c1f979fd
ouloumohand <ooumohand2608@gmail.com>
2026-01-15 01:03:31 +0000

    added pipeline

---
+++ docker-practice/pipeline/pipeline.py
@@ -0,0 +1,17 @@
+#passing argument to command line
+import sys
+
+print("arguments", sys.argv)
+
+month = int(sys.argv[1])
+print(f"Running pipeline for day {month}")
+
+#using pamdas
+#import sys
+import pandas as pd
+
+df = pd.DataFrame({"bikes": [1, 2], "NumberBikes": [3, 4]})
+df['Month'] = month
+print(df.head())
+
+df.to_parquet(f"output_day_{month}.parquet")
